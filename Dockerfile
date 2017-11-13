874997fde809b27fadbdf859ecfe97338cad29ef
thinkernel <thinkernel@gmail.com>
2017-11-13 14:09:35 +0800

    Fix filename

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM redis
-
-RUN mkdir -p /usr/local/etc/redis
-
-COPY ./redis.conf /usr/local/etc/redis/
-
-CMD ["redis-server"]
--- setup-etcd.sh
+++ setup-etcd.sh
@@ -3,7 +3,7 @@ set -e
 
 source env.config 
 
-etcdctl mkdir /haproxy-config/${PROJECT_NAME}/redis
-etcdctl set haproxy-config/${PROJECT_NAME}/redis/frontend/bind *:6379
-etcdctl set haproxy-config/${PROJECT_NAME}/redis/frontend/default_backend redis-server
-etcdctl set haproxy-config/${PROJECT_NAME}/redis/backend/redis-1 redis-server:6379
+etcdctl mkdir /haproxy-config/${COMPOSE_PROJECT_NAME}/redis
+etcdctl set haproxy-config/${COMPOSE_PROJECT_NAME}/redis/frontend/bind *:6379
+etcdctl set haproxy-config/${COMPOSE_PROJECT_NAME}/redis/frontend/default_backend redis-server
+etcdctl set haproxy-config/${COMPOSE_PROJECT_NAME}/redis/backend/redis-1 redis:6379
